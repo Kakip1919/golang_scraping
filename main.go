20c6c49bf5594d7f8a18730995fa93dfe46e978a
DESKTOP-3HOBMRM\OW_KAKUTA <invest9me@yahoo.co.jp>
2022-02-14 22:58:32 +0900

    dev wails dev cryptAPI

--- main.go
+++ main.go
@@ -3,11 +3,45 @@ package main
 import (
 	"fmt"
 	"github.com/sclevine/agouti"
+	"strconv"
+	"time"
 )
 
-func openChromedriver() {
+func scrapingCrypto(page *agouti.Page) {
 
-	driver := agouti.ChromeDriver(agouti.Browser("chrome"))
+	time.Sleep(2 * time.Second)
+	page.FindByXPath("//*[@id=\"tabContainer\"]/div[2]/div[2]/div/div[1]/div[2]/div").Click()
+	cryptData := make(map[string]string)
+
+	for i := 0; i < 5; i++ {
+
+		if i >= 1 {
+			nextButton := fmt.Sprintf("//*[@id=\"tabContainer\"]/div[2]/div[3]/div/button[%s]", strconv.Itoa(i+2))
+			page.FindByXPath(nextButton).Click()
+			time.Sleep(2 * time.Second)
+		}
+		for dataCnt := 1; dataCnt < 20; dataCnt++ {
+			currencyName := fmt.Sprintf("//*[@id=\"tabContainer\"]/div[2]/div[2]/div/div[2]/div[%s]/div/div[1]/div[2]/div", strconv.Itoa(dataCnt))
+			currencyPrice := fmt.Sprintf("//*[@id=\"tabContainer\"]/div[2]/div[2]/div/div[2]/div[%s]/div/div[2]/div", strconv.Itoa(dataCnt))
+			name, _ := page.FindByXPath(currencyName).Text()
+			price, _ := page.FindByXPath(currencyPrice).Text()
+			cryptData[name] = price
+		}
+	}
+	fmt.Println(cryptData)
+}
+
+func openChromedriver() {
+	chromeArgs := agouti.ChromeOptions(
+		"args", []string{
+			"--headless",
+			"--disable-gpu",
+		})
+	chromeExcludeSwitches := agouti.ChromeOptions(
+		"excludeSwitches", []string{
+			"enable-logging",
+		})
+	driver := agouti.ChromeDriver(chromeArgs, chromeExcludeSwitches, agouti.Browser("chrome"))
 
 	if err := driver.Start(); err != nil {
 		fmt.Printf("Failed to start driver. %s\n", err)
@@ -22,8 +56,7 @@ func openChromedriver() {
 		fmt.Printf("Failed to navigate to https://readouble.com/laravel/8.x/ja/eloquent.html. %s\n", err)
 		return
 	}
-
-	page.FindByXPath("//*[@id=\"tabContainer\"]/div[2]/div[2]/div/div[1]/div[2]/div").Click()
+	scrapingCrypto(page)
 }
 
 func main() {
